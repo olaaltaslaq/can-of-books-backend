b689aff16908a4a0381c8b7f9b03f51b44ed0717
root <root@LTUCSTUDENT-L154.localdomain>
2021-08-23 17:53:39 +0300

    lab12

--- server.js
+++ server.js
@@ -1,24 +1,78 @@
-'use strict';
+"use strict";
 
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const jwksClient = require('jwks-rsa');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const axios = require("axios");
+const mongoose = require("mongoose");
 
-const app = express();
-app.use(cors());
+const PORT = process.env.PORT;
+const server = express();
+server.use(cors());
+mongoose.connect("mongodb://localhost:27017/books", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-const PORT = process.env.PORT || 3001;
+//Schema
+const Book = new mongoose.Schema({
+  title: String,
+  description: String,
+  email: String,
+});
 
-app.get('/test', (request, response) => {
+//Model
+const bookModel = mongoose.model("book", Book);
 
-  // TODO: 
-  // STEP 1: get the jwt from the headers
-  // STEP 2. use the jsonwebtoken library to verify that it is a valid jwt
-  // jsonwebtoken dock - https://www.npmjs.com/package/jsonwebtoken
-  // STEP 3: to prove that everything is working correctly, send the opened jwt back to the front-end
 
-})
 
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
+function seedDataCollection() {
+  const book1 = new bookModel({
+    title:
+      "Numbers Don't Lie: 71 Stories to Help Us Understand the Modern World",
+    description:
+      "An essential guide to understanding how numbers reveal the true state of our world—exploring a wide range of topics including energy, the environment, technology, transportation, and food production.",
+    email: "olaaltaslaq@gmail.com",
+  });
+
+  
+
+  const book2 = new bookModel({
+    title: "Blue Ocean Strategy",
+    description:
+      "Drawing on more than a decade of new research, Blue Ocean Shift is the definitive guide to help you move beyond competing, inspire your people’s confidence, and seize new growth.",
+    email: "olaaltaslaq@gmail.com",
+  });
+
+  
+
+  const book3 = new bookModel({
+    title: "A Short History of Nearly Everything",
+    description:
+      "In Bryson's biggest book, he confronts his greatest challenge: to understand—and, if possible, answer—the oldest, biggest questions we have posed about the universe and ourselves.",
+    email: "olaaltaslaq@gmail.com",
+  });
+
+ 
+
+  book1.save();
+  book2.save();
+  book3.save();
+}
+
+seedDataCollection();
+
+
+
+server.get("/books", getData);
+
+function getData(req, res) {
+  bookModel.find(function (err, books) {
+    if (err) return console.error(err);
+    res.send(books);
+  });
+}
+
+server.listen(PORT, () => {
+  console.log(`listening on PORT ${PORT}`);
+});
